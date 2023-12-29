293218be7d57727656f68e7e731e240a35747baf
Lokesh-naik01 <lavadiyalokeshnaik@gmail.com>
2023-12-29 16:24:03 +0530

    return byte slice in jet validate

--- utils/jwt.go
+++ utils/jwt.go
@@ -22,7 +22,7 @@ func VerifyToken(token string)  error{
 		if !ok {
 			return nil, errors.New("Unexpected signin method")
 		}
-		return secretKey, nil
+		return []byte(secretKey), nil
 	})
 	if err!=nil {
 		return errors.New("cant parse token")
