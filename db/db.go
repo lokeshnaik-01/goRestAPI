ff8219b32e335f1bef58278732a724fafd4c5330
Lokesh-naik01 <lavadiyalokeshnaik@gmail.com>
2023-12-29 16:47:32 +0530

    added registration table

--- db/db.go
+++ db/db.go
@@ -42,6 +42,20 @@ func createTables() {
 
 	_, err = DB.Exec(createEventsTable)
 	if err != nil {
-		panic("events table not executed")
+		panic("err")
 	}
+
+	createRegistrationsTable := `
+	CREATE TABLE IF NOT EXISTS registrations(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		event_id INTEGER,
+		user_id INTEGER,
+		FOREIGN KEY(event_id) REFERENCES events(id),
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	)`
+	_, err = DB.Exec(createRegistrationsTable)
+	if err != nil {
+		panic(err)
+	}
+
 }
